52b75df64113dd364e11f1bd5687fb10b339289c
Zourethe <pedresilva4@gmail.com>
2024-01-09 19:56:41 -0300

    feat add: pip updated and new terminal interface

--- env/main.py
+++ env/main.py
@@ -1,8 +1,7 @@
 '''
                                                     Detetec Mouse After Hotkey
     
-    This is the main file of the script, the one that will detect the keys input
-    and execute the basic actions.
+    This is the main file of the script, the one that will detect the keys input and execute the basic actions.
 
     Author: Zourethe
     Date: July, 17, 2023
@@ -16,6 +15,14 @@ shift_l = False
 ctrl_l = False
 toggle = False
 
+# Terminal interface.
+print('\033[32m{}\033[0m'.format('                  Detetec Mouse After Hotkey'))
+print('')
+print('\033[0m{}\033[0m'.format('This is a script that start to detect your mouse click inputs\nafter you press a hotkey (CTRL + SHIFT + F12).'))
+print('')
+print('\033[0m{}\033[0m'.format('Script by Zourethe.'))
+print('')
+
 # On press function.
 def on_press(key):
     global ctrl_l, shift_l, toggle
@@ -25,9 +32,11 @@ def on_press(key):
         shift_l = True
     elif key == keyboard.Key.f12 and ctrl_l and shift_l:
         if toggle == False:
+            print('\033[93m{}\033[0m'.format('Hotkey toggled.'))
             toggle = True
         else:
             toggle = False
+            print('\033[93m{}\033[0m'.format('Hotkey untoggled.'))
 
 # On release function.
 def on_release(key):
@@ -39,8 +48,10 @@ def on_release(key):
 
 # On click function.
 def on_click(x, y, button, pressed):
-    if pressed and toggle:
-        print('Output')
+    if button == mouse.Button.left and toggle:
+        print('\033[32m{}\033[0m'.format('Left click pressed/released.'))
+    if button == mouse.Button.right and toggle:
+        print('\033[31m{}\033[0m'.format('Right click pressed/released.'))
 
 # Pynput listeners.
 kb_lstnr = keyboard.Listener(on_press = on_press, on_release = on_release)
